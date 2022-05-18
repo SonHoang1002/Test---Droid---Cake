76f396a0af449dd9dc3990c1f7e95809dbf33a14
Admin <hoangtrungson07012001@gmail.com>
2022-05-18 22:02:12 +0700

    droid________cafe

--- app/src/main/java/com/example/droid_______cake/OrderActivity.java
+++ app/src/main/java/com/example/droid_______cake/OrderActivity.java
@@ -1,20 +1,87 @@
 package com.example.droid_______cake;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
+
+    TextView tVOrderCake;
+    Spinner spinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
-        Intent intent = getIntent();
-        String message = "Order: " + intent.getStringExtra("cake");
-        TextView textView = findViewById(R.id.order_textview);
-        textView.setText(message);
+        tVOrderCake = findViewById(R.id.tVOrderCake);
+        spinner = findViewById(R.id.spinner);
+
+        Intent i = getIntent();
+        String mes = i.getStringExtra("cake");
+        tVOrderCake.setText(mes);
+        tVOrderCake.setTextSize(30);
+
+        //Spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.arr, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String message = adapterView.getItemAtPosition(i).toString();
+                displayToast(message);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        findViewById(R.id.goToCheckBoxActivity).setOnClickListener(view->{
+
+        });
     }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    // Same day service
+                    displayToast("same_day_messenger_service");
+                break;
+            case R.id.nextday:
+                if (checked)
+                    // Next day delivery
+                    displayToast("next_day_ground_delivery");
+                break;
+            case R.id.pickup:
+                if (checked)
+                    // Pick up
+                    displayToast("pick_up");
+                break;
+            default:
+                // Do nothing.
+                break;
+        }
+    }
+
+
+
+
 }
